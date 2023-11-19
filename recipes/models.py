4e01f7c6ee3be1529101f2b7c73476be654cbe5a
daniel <danielderu@gmail.com>
2023-11-19 14:35:13 +0200

    created models for recipes.

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
-class Image(models.Model):
-    image_url = models.CharField(max_length=1000)
-    
-    def __str__(self) -> str:
-        return self.image_url
+# Create your models here.
     
 class DietLabel(models.Model):
     label = models.CharField(max_length=255)
@@ -23,6 +19,58 @@ class Caution(models.Model):
     
     def __str__(self) -> str:
         return self.caution
+
+class MealType(models.Model):
+    type = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.type
+    
+class CuisineType(models.Model):
+    type = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.type
+    
+class DishType(models.Model):
+    type = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.type
+    
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    recipe_yield = models.FloatField()
+    url = models.CharField(max_length=255)
+    calories = models.FloatField()
+    total_weight = models.FloatField()
+    total_time = models.FloatField()
+    
+    diet_labels = models.ManyToManyField(DietLabel)
+    health_labels = models.ManyToManyField(HealthLabel)
+    cautions = models.ManyToManyField(Caution)
+    meal_type = models.ManyToManyField(MealType)
+    cuisine_type = models.ManyToManyField(CuisineType)
+    dish_type = models.ManyToManyField(DishType)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Nutrient(models.Model):
+    label = models.CharField(max_length=255)
+    total = models.FloatField()
+    daily = models.FloatField()
+    unit = models.CharField(max_length=255)
+    recipe = models.ForeignKey(Recipe)
+    
+
+class SubNutrient(models.Model):
+    label = models.CharField(max_length=255)
+    total = models.FloatField()
+    daily = models.FloatField()
+    unit = models.CharField(max_length=255)
+    nutrient = models.ForeignKey(Nutrient)
     
 class IngredientCategory(models.Model):
     category = models.CharField(max_length=255)
@@ -36,22 +84,20 @@ class Ingredient(models.Model):
     quantity = models.FloatField()
     measure = models.CharField()
     weight = models.FloatField()
-    category = models.CharField()
-    image = models.OneToOneField(Image)
-
-# Create your models here.
-class Recipe(models.Model):
-    name = models.CharField(max_length=255)
-    recipe_yield = models.FloatField()
-    calories = models.FloatField()
-    total_weight = models.FloatField()
-    total_time = models.FloatField()
-    images = models.ManyToManyField(Image)
-    diet_labels = models.ManyToManyField(DietLabel)
-    health_labels = models.ManyToManyField(HealthLabel)
-    cautions = models.ManyToManyField(Caution)
+    
+    category = models.OneToOneField(IngredientCategory)
+    
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
         return self.name
+
+class Image(models.Model):
+    image_url = models.CharField(max_length=1000)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, blank=True, null=True)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, blank=True, null=True)
+    
+    def __str__(self) -> str:
+        return self.image_url
     
     
